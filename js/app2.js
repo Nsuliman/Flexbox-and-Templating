9492d115f3022ecba5423c1a95486c88f8836d60
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-11-17 01:55:20 +0200

    one page website

--- js/app2.js
+++ js/app2.js
@@ -15,22 +15,26 @@ Horns.prototype.render = function() {
   var source   = $('#entry-template').html();
   var template = Handlebars.compile(source);
   var html;
-  Horns.all.forEach( thing =>  {
-    html = template(thing);
-  });
+  // Horns.all.forEach( thing => {
+  html = template(this);
+  // });
   $('#photo-template').append(html);
 };
 
 function populateSelectBox() {
   let seen = {};
-  let select = $('select');
-  Horns.all.forEach( (horn) => {
-    if ( ! seen[horn.keyword] ) {
-      let option = `<option value="${horn.keyword}">${horn.keyword}</option>`;
-      select.append(option);
-      seen[horn.keyword] = true;
+  var source   = $('#options-template').html();
+  var template = Handlebars.compile(source);
+  var array = [];
+  $('select').empty();
+  $('select').html('<option value="default">Filter by Keyword</option>');
+  Horns.all.forEach( (thing) => {
+    if ( ! seen[thing.keyword] ) {
+      array.push(template(thing));
+      seen[thing.keyword] = true;
     }
   });
+  $('select').append(array);
 }
 
 $('select').on('change', function() {
@@ -44,14 +48,31 @@ $('select').on('change', function() {
   }
 });
 
-$.get('../data/page-2.json')
-  .then( data => {
-    data.forEach( (thing) => {
-      let horn = new Horns(thing);
-      horn.render();
-    });
-  })
-  .then( () => populateSelectBox() );
+$('#page2').on('click', function() {
+  $('#photo-template').empty();
+  Horns.all = [];
+  $.get('../data/page-2.json')
+    .then( data => {
+      data.forEach( (thing) => {
+        let horn = new Horns(thing);
+        horn.render();
+      });
+    })
+    .then( () => populateSelectBox() );
+});
+
+$('#page1').on('click', function() {
+  $('#photo-template').empty();
+  Horns.all = [];
+  $.get('../data/page-1.json')
+    .then( data => {
+      data.forEach( (thing) => {
+        let horn = new Horns(thing);
+        horn.render();
+      });
+    })
+    .then( () => populateSelectBox() );
+});
 
 $('button').on('click', function() {
   Horns.all.sort((obj1,obj2) => {return obj1.title < obj2.title ? -1 : 1;});
@@ -62,6 +83,25 @@ $('button').on('click', function() {
   Horns.all.forEach( thing =>{
     html.push(template(thing));
   });
-  // console.log('html : ', html);
   $('#photo-template').append(html);
+});
+
+
+
+
+function showData(pageNum) {
+  $('#photo-template').html('');
+  Horns.all = [];
+  $.get(`../data/page-${pageNum}.json`)
+    .then(data => {
+      data.forEach(thing => {
+        let horn = new Horns(thing);
+        horn.render();
+      });
+    })
+    .then(() => populateSelectBox());
+}
+
+$(document).ready(function () {
+  showData(1);
 });
